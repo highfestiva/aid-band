b3d6138d95b0d2343abdb7dd9be5f08e76433e89
Jonas <highfestiva@gmail.com>
2014-09-30 20:47:15 +0200

    Added support for updating automatically via a bash script.

---
+++ AidBand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+cp --update -R ../AidBandInstall/ ./
+./AidBand.exe
+done
--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ import glob
 import py2exe
 
 playlists = [f for f in glob.glob('*.txt') if 'todo' not in f.lower()]
+files = playlists + ['AidBand.sh']
 
 import py2exe
 py2exe_options = { 'includes': ['pyttsx.drivers.sapi5', 'win32com.gen_py.C866CA3A-32F7-11D2-9602-00C04F8EE628x0x5x4'],
 		   'typelibs': [('{C866CA3A-32F7-11D2-9602-00C04F8EE628}', 0, 5, 4)],
 		   'optimize': 2 }
 
-setup(console=['aidband.py'], data_files=[('', playlists)], options = {'py2exe': py2exe_options})
+setup(console=['aidband.py'], data_files=[('', files)], options = {'py2exe': py2exe_options})
