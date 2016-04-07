eb6cf502447f7f7a50d3900fdf0d6d3fef17446b
Jonas B <jonas.bystrom@hm.com>
2016-04-07 14:16:19 +0200

    Update for new Spotify Charts web api.

--- toplist.py
+++ toplist.py
@@ -3,12 +3,18 @@
 from urllib.request import urlopen
 
 def search(country='se', count=50):
-	url = 'https://spotifycharts.com/api/?recurrence=weekly&date=latest&offset=0&limit=%i&type=regional&country=%s' % (count, country)
+	url = 'http://spotifycharts.com/api/?recurrence=weekly&date=latest&offset=0&limit=%i&type=regional&country=%s' % (count, country)
 	json = urlopen(url).read().decode()
 	py = json.replace('true','True').replace('false','False').replace('null','None')
 	toplist_dict = eval(py)
 	result = []
-	return [entry['track'] for entry in toplist_dict['entries']['items']]
+	for e in toplist_dict['entries']['items']:
+		track = e['track']
+		result += [{	'name':		track['name'],
+				'popularity':	e['plays'],
+				'artists':	track['artists'],
+				'uri':		'spotify:track:'+track['id'],	}]
+	return result
 
 if __name__ == '__main__':
 	for track in search():
