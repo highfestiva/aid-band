fd54927d235fbe22d43476da1391a5419148dd60
Jonte <highfestiva@gmail.com>
2017-08-13 23:13:22 +0200

    Fix Google->YouTube searches

--- youtube_radio.py
+++ youtube_radio.py
@@ -9,7 +9,9 @@ import subprocess
 import urllib.parse
 
 
-pages = re.compile(r'(http.*?://[a-z\.-_]*?youtube\.com/watch?v=.+)".*?>(.*?)<')
+pages = re.compile(r'q=(http.*?://[a-z\.-_]*?youtube\.com/watch%3Fv%3D.+?)&.*?>(.*?)</a>')
+tags = re.compile(r'<.*?>')
+parenths = re.compile(r'\(.*?\)')
 
 
 def search(s):
@@ -19,17 +21,26 @@ def search(s):
     html = subprocess.check_output('curl -H "user-agent: Mozilla/5.0" %s' % url, shell=True, stderr=subprocess.DEVNULL).decode()
     artist = ''
     songs = []
+    urls = set()
+    names = set()
     for pagelink in pages.finditer(html):
-        url = pagelink.group(1)
-        name = pagelink.group(2)
+        url = pagelink.group(1).replace('%3F','?').replace('%3D', '=')
+        name = parenths.sub('', tags.sub('', pagelink.group(2)))
+        name = parenths.sub('', name)
+        name = ' '.join(name.split()).strip()
+        exists = (url in urls or name in names)
+        urls.add(url)
+        names.add(name)
+        if exists:
+            continue
         words = [w.strip() for w in name.split('-') if 'youtube' not in w.lower()]
         if len(words) == 2:
             artist = words[0]
-            song = words[1].split('(')[0]
+            song = words[1]
             songs += [ABSong(song, artist, url)]
         elif len(words) == 1:
             song = words[0]
-            artist_index = artist.lower() in song.lower().find(artist.lower())
+            artist_index = song.lower().find(artist.lower())
             if artist and artist_index >= 0:
                 song = song[:artist_index].strip() + ' ' + song[artist_index+len(artist):].strip()
                 songs += [ABSong(song, artist, url)]
@@ -39,7 +50,7 @@ def search(s):
 
 
 def cache_song(url, wildcard):
-    if 'https://youtube.com' in url:
+    if 'youtube.com' in url:
         video = pafy.new(url)
         audiostream = max(video.audiostreams, key=lambda audio: abs(audio.rawbitrate-131072))
         src_filename = audiostream.download()
@@ -50,4 +61,5 @@ def cache_song(url, wildcard):
 
 
 songs = search('Kim Cesarion')
-cache_song(songs[0], './something.*')
+print(songs)
+cache_song(songs[0].uri, './something.*')
