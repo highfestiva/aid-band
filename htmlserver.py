811d7f4d1255ec79cecaa346b23a20d5fc32335f
Jonte <highfestiva@gmail.com>
2020-10-12 12:08:25 +0200

    .m4a support by passing content-length in web server

--- htmlserver.py
+++ htmlserver.py
@@ -31,12 +31,14 @@ class AidbandServer(BaseHTTPRequestHandler):
                 song = ABSong(line)
                 wc = util._cachewildcard(song)
                 for fn in glob(wc):
-                    print(song)
+                    print('Web server returning "%s"' % song)
                     self.send_response(200)
                     mime = mimetypes[os.path.splitext(fn)[1][1:].lower()]
+                    data = open(fn, 'rb').read()
                     self.send_header('Content-Type', mime)
+                    self.send_header('Content-Length', len(data))
                     self.end_headers()
-                    self.wfile.write(open(fn, 'rb').read())
+                    self.wfile.write(data)
                     return
         self.send_response(404)
         self.end_headers()
